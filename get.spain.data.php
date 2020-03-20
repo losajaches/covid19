91324fc261422ddd7858900b6c2b0ec4321fd837
losajaches <34785719+losajaches@users.noreply.github.com>
2020-03-20 06:09:35 +0000

    Create get.spain.data.php

---
+++ get.spain.data.php
@@ -0,0 +1,288 @@
+<?php
+//https://www.mscbs.gob.es/profesionales/saludPublica/ccayes/alertasActual/nCov-China/documentos/Actualizacion_49_COVID-19.pdf
+function GetSpainData(){
+	$data="
+	18
+	Andalucía 1.008 11,84 25 23
+	Aragón 281 21,22 14 15
+	Asturias 292 28,06 6 1
+	Baleares 169 14,18 7 2
+	Canarias 220 9,84 23 3
+	Cantabria 83 12,56 4 1
+	Castilla-La Mancha 801 38,76 37 42
+	Castilla y León 868 35,72 54 29
+	Cataluña 2.702 34,89 33 55
+	Ceuta 1 1,18 0 0
+	C. Valenciana 921 18,03 52 24
+	Extremadura 241 22,01 3 8
+	Galicia 453 16,74 15 4
+	Madrid 6.777 100,35 590 498
+	Melilla 23 26,59 0 0
+	Murcia 167 11,18 6 0
+	Navarra 482 73,22 11 4
+	País Vasco 1.190 53,13 44 53
+	La Rioja 468 144,26 15 5
+	Total 17.147 35,96 939 767
+	17
+	Andalucía 859 10,07 21 19
+	Aragón 226 17,13 9 13
+	Asturias 242 23,46 5 1
+	Baleares 112 9,31 4 1
+	Canarias 181 8,08 17 3
+	Cantabria 68 9,98 3 1
+	Castilla-La Mancha 675 32,61 29 28
+	Castilla y León 668 27,38 43 22
+	Cataluña 1.866 24,12 33 41
+	Ceuta 1 1,18 0 0
+	C. Valenciana 726 14,13 40 22
+	Extremadura 194 17,61 3 6
+	Galicia 341 12,59 12 3
+	Madrid 5637 83,55 491 390
+	Melilla 20 23,12 0 0
+	Murcia 122 8,17 4 0
+	Navarra 386 58,54 10 3
+	País Vasco 973 43,30 37 40
+	La Rioja 419 130,05 13 5
+	Total 13.716 28,75 774 598
+	16
+	Andalucía 683 7,97 13 11
+	Aragón 207 15,69 9 12
+	Asturias 193 18,77 5 1
+	Baleares 92 7,83 4 1
+	Canarias 148 6,55 13 2
+	Cantabria 58 8,26 2 0
+	Castilla-La Mancha 567 27,55 23 17
+	Castilla y León 431 17,63 24 12
+	Cataluña 1.394 17,97 33 18
+	Ceuta 1 1,18 0 0
+	C. Valenciana 541 10,51 27 13
+	Extremadura 153 13,77 2 3
+	Galicia 292 10,82 9 3
+	Madrid 4.871 72,37 340 355
+	Melilla 17 19,66 0 0
+	Murcia 97 6,49 3 0
+	Navarra 313 47,69 10 2
+	País Vasco 765 34,06 33 36
+	La Rioja 355 111,11 13 5
+	Total 11.178 23,45 563 491
+	13
+	Andalucía 219 2,53 8 0
+	Aragón 80 6,06 3 7
+	Asturias 67 6,55 2 1
+	Baleares 30 2,52 2 1
+	Canarias 70 2,97 2 0
+	Cantabria 29 4,99 0 0
+	Castilla-La Mancha 194 9,54 10 5
+	Castilla y León 169 6,96 8 1
+	Cataluña 316 4,08 25 4
+	Ceuta 0 0,00 0 0
+	C. Valenciana 94 1,72 2 1
+	Extremadura 39 3,65 1 1
+	Galicia 85 3,15 2 0
+	Madrid 1990 29,79 180 81
+	Melilla 2 2,31 0 0
+	Murcia 35 2,34 1 0
+	Navarra 130 19,87 4 0
+	País Vasco 417 18,89 20 14
+	La Rioja 243 76,71 2 2
+	Total 4.209 8,99 272 120
+	12
+	Andalucía 115 1,35 4 0
+	Aragón 64 4,85 3 6
+	Asturias 47 4,60 2 1
+	Baleares 22 1,83 2 1
+	Canarias 51 2,09 3 0
+	Cantabria 16 2,75 0 0
+	Castilla-La Mancha 115 5,66 9 1
+	Castilla y León 92 3,83 4 0
+	Cataluña 260 3,36 0 4
+	Ceuta 0 0,00 0 0
+	C. Valenciana 76 1,48 2 1
+	Extremadura 19 1,78 1 1
+	Galicia 35 1,30 2 0
+	Madrid 1388 20,77 135 56
+	Melilla 0 0,00 0 0
+	Murcia 26 1,74 1 0
+	Navarra 73 11,16 3 0
+	País Vasco 346 15,67 18 11
+	La Rioja 205 64,71 1 2
+	Total 2.950 6,29 190 84
+	11
+	Andalucía 90 1,07 3 0
+	Aragón 45 3,41 3 4
+	Asturias 32 3,13 2 0
+	Baleares 16 1,39 1 0
+	Canarias 37 1,49 1 0
+	Cantabria 12 2,07 0 0
+	Castilla-La Mancha 71 3,49 5 0
+	Castilla y León 71 2,96 2 0
+	Cataluña 156 2,02 0 3
+	Ceuta 0 0,00 0 0
+	C. Valenciana 65 1,28 1 1
+	Extremadura 9 0,84 1 0
+	Galicia 39 1,44 2 0
+	Madrid 1024 15,34 102 31
+	Melilla 0 0,00 0 0
+	Murcia 11 0,74 0 0
+	Navarra 46 7,03 3 0
+	País Vasco 225 10,19 15 6
+	La Rioja 179 56,50 1 2
+	Total 2128 4,55 142 47
+	10
+	Andalucía 71 0,84 1 0
+	Aragón 38 2,88 3 3
+	Asturias 22 2,15 2 0
+	Baleares 13 1,13 1 0
+	Canarias 25 1,16 1 0
+	Cantabria 12 2,07 0 0
+	Castilla La Mancha 39 1,92 2 0
+	Castilla y León 56 2,33 2 0
+	Cataluña 124 1,62 0 3
+	Ceuta 0 0,00 0 0
+	C. Valenciana 50 1,00 1 1
+	Extremadura 8 0,75 0 0
+	Galicia 22 0,81 2 0
+	Madrid 782 11,74 77 21
+	Melilla 0 0,00 0 0
+	Murcia 9 0,60 0 0
+	Navarra 12 1,83 2 0
+	País Vasco 195 8,83 5 6
+	La Rioja 144 45,45 1 1
+	Total 1.622 3,48 100 35
+	9
+	Andalucía 54 0,64 1 0
+	Aragón 13 0,99 1 1
+	Asturias 7 0,68 2 0
+	Baleares 11 0,96 0 0
+	Canarias 22 1,02 0 0
+	Cantabria 12 2,07 0 0
+	Castilla La Mancha 26 1,28 2 0
+	Castilla y León 23 0,96 1 0
+	Cataluña 75 0,98 0 1
+	Ceuta 0 0,00 0 0
+	C. Valenciana 37 0,74 1 1
+	Extremadura 7 0,66 0 0
+	Galicia 6 0,22 0 0
+	Madrid 469 7,04 53 8
+	Melilla 0 0,00 0 0
+	Murcia 4 0,27 0 0
+	Navarra 3 0,46 1 0
+	País Vasco 149 6,75 5 5
+	La Rioja 81 25,57 1 0
+	Total 999 2,14 68 16
+	6
+	Andalucía 21 0,25 1 0
+	Aragón 6 0,45 1 1
+	Asturias 5 0,49 2 0
+	Baleares 6 0,52 0 0
+	Canarias 11 0,51 0 0
+	Cantabria 10 1,72 0 0
+	Castilla La Mancha 15 0,74 1 0
+	Castilla y León 14 0,58 1 0
+	Cataluña 24 0,31 0 0
+	Ceuta 0 0,00 0 0
+	C. Valenciana 30 0,60 1 1
+	Extremadura 6 0,56 0 0
+	Galicia 3 0,11 0 0
+	Madrid 137 2,06 2 2
+	Melilla 0 0,00 0 0
+	Murcia 0 0,00 0 0
+	Navarra 3 0,46 1 0
+	País Vasco 45 2,04 1 1
+	La Rioja 29 9,15 0 0
+	Total 365 0,78 11 5
+	5
+	Andalucía 12 0,14 1 0
+	Aragón 1 0,08 1 0
+	Asturias 5 0,49 2 0
+	Baleares 6 0,52 0 0
+	Canarias 8 0,37 0 0
+	Cantabria 10 1,72 0 0
+	Castilla La Mancha 13 0,64 1 0
+	Castilla y León 11 0,46 1 0
+	Cataluña 24 0,31 0 0
+	Ceuta 0 0,00 0 0
+	C. Valenciana 19 0,38 0 1
+	Extremadura 6 0,56 0 0
+	Galicia 1 0,04 0 0
+	Madrid 90 1,35 2 1
+	Melilla 0 0,00 0 0
+	Murcia 0 0,00 0 0
+	Navarra 3 0,46 1 0
+	País Vasco 17 0,77 0 1
+	La Rioja 11 3,47 0 0
+	Total 237 0,51 9 3
+	4
+	Andalucía 13 0,15 1
+	Aragón 0 0,00
+	Asturias 2 0,20 1
+	Baleares 5 0,43
+	Canarias 7 0,33
+	Cantabria 10 1,72
+	Castilla La Mancha 12 0,59 1
+	Castilla y León 11 0,46 1
+	Cataluña 15 0,20
+	Ceuta 0 0,00
+	C. Valenciana 19 0,38 1
+	Extremadura 6 0,56
+	Galicia 1 0,04
+	Madrid 70 1,05 2
+	Melilla 0 0,00
+	Murcia 0 0,00
+	Navarra 3 0,46 1
+	País Vasco 17 0,77
+	La Rioja 7 2,21
+	Total 198 0,42 7 1
+	";
+	$data=explode("\n",$data);
+	$bigdata=array();
+	$fallecidos=array();
+	$contagiados=array();
+	$fechas=array();
+	foreach($data as $s){
+		$s=trim($s);
+		if($s!=""){
+			if(strlen($s)<=2){
+				$key=$s;
+				$bigdata[$key]=array();
+			}else{
+				$s=str_replace("País Vasco","País·Vasco",$s);
+				$s=str_replace("La Rioja","La·Rioja",$s);
+				$s=str_replace("C. Valenciana","C.·Valenciana",$s);
+				$s=str_replace("Castilla y León","Castilla·y·León",$s);
+				$s=str_replace("Castilla La Mancha","Castilla·La·Mancha",$s);
+				$s=str_replace("Castilla-La Mancha","Castilla·La·Mancha",$s);
+				$s=str_replace(".","",$s);
+				$s=str_replace(",",".",$s);
+				//$s=str_replace("","",$s);
+				$s=explode(" ",$s);
+				$ccaa=$s[0];
+				$bigdata[$key][$ccaa]=array("t"=>$s[1],"f"=>isset($s[4])?$s[4]:0);
+				if(!isset($fallecidos[$ccaa])){
+					$fallecidos[$ccaa]=array();	
+				}
+				$fechas[$key]="3/$key/20";
+				$fallecidos[$ccaa][]=isset($s[4])?$s[4]:0;
+				$contagiados[$ccaa][]=$s[1];
+			}
+		}
+	}
+	foreach($fallecidos as $ccaa=>$s){
+		$fallecidos[$ccaa]=implode(",",array_reverse($s));
+	}
+	foreach($contagiados as $ccaa=>$s){
+		$contagiados[$ccaa]=implode(",",array_reverse($s));
+	}
+	$data_fallecidos=array();
+	$data_fallecidos[]="Province/State,Country/Region,Lat,Long,".implode(",",array_reverse($fechas));
+	foreach($fallecidos as $ccaa=>$linea){
+		$data_fallecidos[]=",".str_replace("·"," ",$ccaa).",0,0,$linea";
+	}
+	$data_contagiados=array();
+	$data_contagiados[]="Province/State,Country/Region,Lat,Long,".implode(",",array_reverse($fechas));
+	foreach($contagiados as $ccaa=>$linea){
+		$data_contagiados[]=",".str_replace("·"," ",$ccaa).",0,0,$linea";
+	}
+	return array("fallecidos"=>$data_fallecidos,"contagiados"=>$data_contagiados);
+}
+?>
