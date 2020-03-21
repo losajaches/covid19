5ecfd40039169ab18489e340a4e95b6d6232fa91
losajaches <34785719+losajaches@users.noreply.github.com>
2020-03-21 08:25:42 +0000

    Update index.php

--- index.php
+++ index.php
@@ -151,9 +151,15 @@ foreach($data_fallecidos as $pais=>$data){
 <html lang="es">
   <head>
     <meta charset="utf-8">
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name='apple-mobile-web-app-capable' content='yes' />
+    <meta name='apple-mobile-web-app-title' content='COVID-19'>
     <meta name="description" content="">
     <meta name="author" content="PpSoft">
+	<link rel='icon' type='image/png' href='covid19.png'/>
+	<link rel='apple-touch-icon' sizes='256x256' href='covid19.png'>
+    
     <title>COVID-19</title>
 	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<style>
